e75b32dcebde71c1eed16f30ca3334291b1cbcfd
Matt <mdagostino175@gmail.com>
2019-02-13 10:36:29 +1000

    Added purge command

--- bot.py
+++ bot.py
@@ -74,6 +74,15 @@ async def allroles(ctx):
         message += role.name + " : " + role.id + "\n"
     await client.say(message)
 
+@client.command(pass_context=True)
+async def clear(ctx, amount=100):
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit=int(amount) + 1):
+        messages.append(message)
+    await client.delete_messages(messages)
+    await client.say('Messages deleted')
+
 
 @client.event
 async def on_message(message):
